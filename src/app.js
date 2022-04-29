7fe66b88015f1d969132cb69b485eec061843274
lucasmp <lucasmatiasperez@gmail.com>
2022-04-29 16:26:04 -0300

    Refactoriza el template

--- src/app.js
+++ src/app.js
@@ -22,16 +22,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  const data = fs.readFileSync('./data/equipos.json');
-  const teams = JSON.parse(data);  
-  res.render('home', { teams });
+  try {
+    const data = fs.readFileSync('./data/equipos.json');
+    const teams = JSON.parse(data);
+    res.render('home', { teams });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: 'something blew up' });
+  }
 });
 
-app.get('/club/:tla', (req, res) => {
-  console.log(req.params)
-  const data = fs.readFileSync(`./data/equipos/${req.params.tla}.json`);
-  const team = JSON.parse(data);
-  res.render('club', { team });
+app.get('/club/:id', (req, res) => {
+  try {
+    const data = fs.readFileSync(`./data/equipos.json`);
+    const teams = JSON.parse(data);
+    const team = teams.find(team => team.id === Number(req.params.id));
+    res.render('club', { team });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/')
+  }
 });
 
 app.get('/create', (req, res) => {
@@ -39,128 +49,116 @@ app.get('/create', (req, res) => {
 });
 
 app.post('/create', upload.single('shield'), (req, res) => {
-
-  /* Toma los datos del form y crea un objeto newTeam */
-  let newTeam = JSON.parse(JSON.stringify(req.body));
-  newTeam.tla = newTeam.name.slice(0, 3).toUpperCase();
-
-  if (typeof req.file !== 'undefined') {
-    newTeam.crestUrl = `../${req.file.path}`;
-  }
-
-  /* Crea un archivo con la información del newTeam */
   try {
-    fs.writeFileSync(`./data/equipos/${newTeam.tla}.json`, JSON.stringify(newTeam));
-    console.log('The file was created successfully!');
-  } catch (err) {
-    console.error(err);
-  }
-
-  /* Agreaga newTeam al archivo 'equipos.json' sobreescribiendolo */
-  const data = fs.readFileSync('./data/equipos.json');
-  const teams = JSON.parse(data);
-  teams.push(newTeam);
-  
-  try {
-    fs.writeFileSync('./data/equipos.json', JSON.stringify(teams));
-    console.log('The team was appended to file!');
-  } catch (err) {
-    console.error(err);
-  }
-
-  res.render('create', {
-    data: {
-      message: 'The team was created successfully',
-      tlaNewTeam: newTeam.tla
-    }
-  });
-});
-
-
-app.get('/delete/:tla', (req, res) => {
-  
-  const data = fs.readFileSync('./data/equipos.json');
-  const teams = JSON.parse(data);
-
-  const tlaTeam = req.params.tla;
-
-  try {
-    fs.unlinkSync(`./data/equipos/${tlaTeam}.json`);
-    console.log('File deleted!');
-  } catch (err) {
-    console.error(err);
-  }
-
-  const team = teams.find(team => team.tla === tlaTeam)
-  try {
-    fs.unlinkSync(team.crestUrl.slice(3));
-    console.log('Image deleted!');
-  } catch (err) {
-    console.error(err);
-  }
-
-  const newTeams = teams.filter(team => team.tla !== tlaTeam);
-  try {
-    fs.writeFileSync('./data/equipos.json', JSON.stringify(newTeams));
-    console.log('The team was deleted from file!');
-  } catch (err) {
-    console.error(err);
-  }
-
-  res.redirect('/'); 
-});
-
-app.get('/edit/:tla', (req, res) => {
-  const data = fs.readFileSync(`./data/equipos/${req.params.tla}.json`);
-  const team = JSON.parse(data);
-
-  res.render('edit', {
-    data: {
-      team,
-      foundedIsNotEmpty: !isNaN(team.founded)
-    }
-  });
-});
-
-app.post('/edit', upload.single('shield'), (req, res) => {
-
-    /* Toma los datos del form y crea un objeto newTeam */
-    console.log(req.body)
-    let newTeam = JSON.parse(JSON.stringify(req.body));
-    newTeam.tla = newTeam.name.slice(0, 3).toUpperCase();
-  
-    if (typeof req.file !== 'undefined') {
-      newTeam.crestUrl = `../${req.file.path}`;
-    }
-  
-    /* Crea (sobreescribe) un archivo con la información del newTeam */
-    try {
-      fs.writeFileSync(`./data/equipos/${newTeam.tla}.json`, JSON.stringify(newTeam));
-      console.log('The file was edited successfully!');
-    } catch (err) {
-      console.error(err);
-    }
-  
-    /* Agreaga newTeam al archivo 'equipos.json' sobreescribiendolo */
     const data = fs.readFileSync('./data/equipos.json');
     const teams = JSON.parse(data);
+    const newTeam = {
+      id: Math.floor((Math.random() * 2000) + 1000),
+      area: {
+        id: Math.floor((Math.random() * 200) + 1),
+        name: req.body.country
+      },
+      name: req.body.name,
+      shortName: req.body.name.split(' ')[0],
+      tla: req.body.name.slice(0, 3).toUpperCase(),
+      crestUrl: req.file ? `../${req.file.path}` : '../data/empty-shield.png',
+      address: req.body.address,
+      phone: req.body.phone,
+      website: req.body.website,
+      email: req.body.email,
+      founded: req.body.founded,
+      clubColors: '',
+      venue: req.body.venue,
+      lastUpdated: new Date().toISOString()
+    };
     teams.push(newTeam);
-    
-    try {
-      fs.writeFileSync('./data/equipos.json', JSON.stringify(teams));
-      console.log('The team was appended to file!');
-    } catch (err) {
-      console.error(err);
+    fs.writeFileSync('./data/equipos.json', JSON.stringify(teams));
+    console.log('The team was appended to file!');
+    res.render('create', {
+      data: {
+        message: 'The team was created successfully',
+        id: newTeam.id
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: 'something blew up' });
+  }
+});
+
+app.get('/edit/:id', (req, res) => {
+  try {
+    const data = fs.readFileSync(`./data/equipos.json`);
+    const teams = JSON.parse(data);
+    const team = teams.find(team => team.id === Number(req.params.id));
+    res.render('edit', { team });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+});
+
+app.post('/edit/:id', upload.single('shield'), (req, res) => {
+  try {
+    const data = fs.readFileSync('./data/equipos.json');
+    const teams = JSON.parse(data);
+    const actualTeam = teams.find(team => team.id === Number(req.params.id));
+    const indexTeam = teams.findIndex(team => team.id === Number(req.params.id));
+    const teamEdited = {
+      id: Number(req.params.id),
+      area: {
+        id: Math.floor((Math.random() * 200) + 1),
+        name: req.body.country
+      },
+      name: req.body.name,
+      shortName: req.body.name.split(' ')[0],
+      tla: req.body.name.slice(0, 3).toUpperCase(),
+      crestUrl: req.file ? `../${req.file.path}` : '../data/empty-shield.png',
+      address: req.body.address,
+      phone: req.body.phone,
+      website: req.body.website,
+      email: req.body.email,
+      founded: req.body.founded,
+      clubColors: '',
+      venue: req.body.venue,
+      lastUpdated: new Date().toISOString()
+    };
+    teams.splice(indexTeam, 1, teamEdited);
+    if (/^\.\.\/uploads\/imagenes/.test(actualTeam.crestUrl)) {
+      fs.unlinkSync(actualTeam.crestUrl.slice(3));
+      console.log('Image deleted!');
     }
+    fs.writeFileSync(`./data/equipos.json`, JSON.stringify(teams));
+    console.log('The file was edited successfully!');
+    res.render('edit', {
+      data: {
+        message: 'The team was edited successfully',
+        id: teamEdited.id
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'something blew up' });
+  }
+});
 
-
-  res.render('edit', {
-    data: {
-      message: 'The team was edited successfully',
-      tlaNewTeam: newTeam.tla
+app.get('/delete/:id', (req, res) => {
+  try {
+    const data = fs.readFileSync('./data/equipos.json');
+    const teams = JSON.parse(data);
+    const teamDelet = teams.find(team => team.id === Number(req.params.id));
+    const newTeams = teams.filter(team => team.id !== Number(req.params.id));
+    if (/^\.\.\/uploads\/imagenes/.test(teamDelet.crestUrl)) {
+      fs.unlinkSync(teamDelet.crestUrl.slice(3));
+      console.log('Image deleted!');
     }
-  });
-
+    fs.writeFileSync('./data/equipos.json', JSON.stringify(newTeams));
+    console.log('The team was deleted from file!');
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'something blew up' });
+  }
 });
 
 app.listen(PUERTO);
